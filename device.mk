fac0a55cec8503d7a96230befd3069d7c5aba986
Mustvfa <mostafamostafamohamed133@gmail.com>
2025-08-25 15:16:28 +0300

    Derpfest bringup

--- device.mk
+++ device.mk
@@ -22,3 +22,7 @@ $(call inherit-product, vendor/samsung/a21s/a21s-vendor.mk)
 
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
+
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay
--- lineage_a21s.mk
+++ lineage_a21s.mk
@@ -34,7 +34,7 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 PRODUCT_DEVICE := a21s
 PRODUCT_NAME := lineage_a21s
 PRODUCT_BRAND := samsung
-PRODUCT_MODEL := SM-A217X
+PRODUCT_MODEL := SM-A217F
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_SHIPPING_API_LEVEL := 29
 
