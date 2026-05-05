74ab224ccafd3da563f39508dcbbf9f6c6ed041f
mustvfa <118094847+mustvfa@users.noreply.github.com>
2026-05-05 08:08:36 +0300

    nuke gapps

--- infinity_a21s.mk
+++ infinity_a21s.mk
@@ -34,7 +34,7 @@ $(call inherit-product, vendor/infinity/config/common_full_phone.mk)
 INFINITY_MAINTAINER := Mustafa
 
 # Whether Including Google Apps
-WITH_GAPPS := true
+WITH_GAPPS := false
 
 ## Device identifier, this must come after all inclusions
 PRODUCT_DEVICE := a21s
